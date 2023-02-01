7d69dee6349a2bebb999094dfcad5dcfd7691d9c
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-02-01 07:49:44 +0200

    fix import

--- src/main/java/ua/shpp/gpt/controllers/ControllerGPT.java
+++ src/main/java/ua/shpp/gpt/controllers/ControllerGPT.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ua.com.shpp.models.Echo;
+import ua.shpp.gpt.models.Echo;
 
 @RestController
 @RequestMapping(value = "/GPT", produces = "application/json")
